6dec3aaed10e3fdb43e714d0d4c4cfddbc5e6f98
kaidenkennedy7 <103951147+kaidenkennedy7@users.noreply.github.com>
2022-06-21 21:49:37 -0400

    DOM exercises

---
+++ unit7DOM.js
@@ -0,0 +1,32 @@
+//Select without using querySelector
+document.getElementById('container');
+//Select section using querySelector
+document.querySelector('#container');
+//Select all of the list items with the class of "second"
+document.querySelectorAll('.second');
+//Select a list item w a class of third, but only the list item inside of the ol tag
+document.querySelector('ol .third');
+//Give the section with an id of container the text "Hello!"
+let foundDiv = document.querySelector('#container');
+foundDiv.innerText = 'Hello!';
+//Add the class main to the div with a class of footer
+let footer = document.querySelector('.footer');
+footer.classList.add('main');
+//Remove the class main on the div with a class of footer
+let footer = document.querySelector('.footer');
+footer.classList.remove('main');
+//Create a new li element
+let newLi = document.createElement('li');
+//Give the new li the text "four"
+newLi.innerText = 'four';
+//Append the li to the ul element
+let list = document.querySelector('ul');
+list.appendChild(newLi);
+//Loop over all of the lis inside the ol tage and give them a background color of green
+let liInsideOl = document.querySelectorAll('ol li');
+for (let i = 0; i < liInsideOl.length; i++) {
+	liInsideOl[i].style.backgroundColor = 'green';
+}
+//Remove the div with a class of footer
+let footer = document.querySelector('.footer');
+footer.remove();
